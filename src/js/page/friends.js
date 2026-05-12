1c8257bddce1b4bd1b281d5ac1261ec8645e02eb
shell <40906916+biliblihuorong@users.noreply.github.com>
2026-05-12 11:52:00 +0800

    feat: 适配 plugin-friends 和 halo-plugin-steam 到 Butterfly 主题

---
+++ src/js/page/friends.js
@@ -0,0 +1,14 @@
+/**
+ * @date: 2026/5/12
+ * @author: shell
+ * @fileName: friends
+ * @Description: 朋友圈
+ */
+
+import App from '../core/App';
+import Pagination from '../modules/Pagination';
+
+@App([Pagination])
+class Friends {
+
+}
---
+++ src/js/page/steam.js
@@ -0,0 +1,14 @@
+/**
+ * @date: 2026/5/12
+ * @author: shell
+ * @fileName: steam
+ * @Description: Steam 游戏展示
+ */
+
+import App from '../core/App';
+import Pagination from '../modules/Pagination';
+
+@App([Pagination])
+class Steam {
+
+}
